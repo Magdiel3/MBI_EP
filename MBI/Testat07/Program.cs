259b2d61d3fa866831755d1b925a5a56bba18ffa
Magdiel Martínez <magdieltercero@hotmail.com>
2020-03-04 21:11:34 +0100

    Update Program.cs

--- MBI/Testat07/Program.cs
+++ MBI/Testat07/Program.cs
@@ -1,12 +1,27 @@
-﻿using System;
+using System;
 
-namespace Testat07
+namespace Aufgabe7
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("\nAufgabe 7");
+            Console.WriteLine("Bitte eine Ganzzahl, deren Teilbarkeit getestet werden soll, eingeben und mit Enter bestätigen: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Bitte einen ganzzahligen Teiler eingeben und mit Enter bestätigen: ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            if(num1 % num2 == 0)
+            {
+
+                Console.WriteLine(num1 +  " ist ohne Rest durch " + num2 + " teilbar");       
+            }
+            else
+            {
+                Console.WriteLine(num1 + " ist nicht durch " + num2 + " teilbar"); 
+            }
         }
     }
 }
