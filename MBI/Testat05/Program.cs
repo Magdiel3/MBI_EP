16844fc9ae0131c47552873749d2315e4e5c30ae
Magdiel Martínez <magdieltercero@hotmail.com>
2020-03-04 20:49:19 +0100

    Update Program.cs

--- MBI/Testat05/Program.cs
+++ MBI/Testat05/Program.cs
@@ -1,12 +1,25 @@
-﻿using System;
+using System;
 
-namespace Testat05
+namespace program
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("\nAufgabe 5");
+            Console.WriteLine("Bitte einen Radius eingeben une mit Enter bestätigen :");
+            double radius = Convert.ToDouble(Console.ReadLine());
+            double pi;
+            pi = Math.PI ;
+            double durchmesser, umfang, fläche;
+            durchmesser = 2*radius;
+            umfang = pi*durchmesser;
+            fläche = pi*radius*radius;
+
+            Console.WriteLine("Für den Radius = " + Math.Round(radius,2));
+            Console.WriteLine("Durchmesser = " + Math.Round(durchmesser,2));
+            Console.WriteLine("Umfang = " + Math.Round(umfang,2));
+            Console.WriteLine("Fläche = " + Math.Round(fläche,2));
         }
     }
 }
