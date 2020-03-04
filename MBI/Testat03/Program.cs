ed6dc25f256dba3fbf997a7f35197ab3f81797f2
Magdiel Martínez <magdieltercero@hotmail.com>
2020-03-04 12:56:49 +0100

    Start Commit
    Blank projects

---
+++ MBI/Testat03/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Testat03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
