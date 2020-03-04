b0f2b51cfd61611c9086013b623cd2c0131d18f1
Magdiel Martínez <magdieltercero@hotmail.com>
2020-03-04 20:29:44 +0100

    Update Program.cs

--- MBI/Testat03/Program.cs
+++ MBI/Testat03/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace Testat03
 {
@@ -6,7 +6,27 @@ namespace Testat03
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Aufgabe 3");
+
+            double x = 3.5, y = 4.6, ergebnis;
+
+            Console.WriteLine("x = " + x + " y = " + y);
+            Console.Write("Das Ergebnis der Addition x + y = ");
+            ergebnis = x + y;
+            Console.WriteLine(ergebnis);
+			
+
+            Console.Write("Das Ergebnis der Substraktion x - y = ");
+            ergebnis = x - y;
+            Console.WriteLine(ergebnis);
+
+            Console.Write("Das Ergebnis der Multiplikation x * y = ");
+            ergebnis = x + y;
+            Console.WriteLine(ergebnis);
+
+            Console.Write("Das Ergebnis der Division x / y = ");
+            ergebnis = x / y;
+            Console.WriteLine(ergebnis);
         }
     }
 }
