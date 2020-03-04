ee56153f5e9deda0266cfdec64c48e1ea42d7151
Magdiel Martínez <magdieltercero@hotmail.com>
2020-03-04 20:20:02 +0100

    Update Program.cs

--- MBI/Testat02/Program.cs
+++ MBI/Testat02/Program.cs
@@ -1,32 +1,33 @@
 using System;
 
-namespace Testat2
+namespace Aufgabe2
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int num = 1;
-            Console.WriteLine("i = " + num);
+            printAufgabe("2.1");
+            int zahl = 1;
+            Console.WriteLine("i = " + zahl);
 
             Console.ReadLine();
-
+            printAufgabe("2.2");
             Console.WriteLine("Bitte eine Zahl eingeben und mit Enter bestätigen");
             string text = Console.ReadLine();
-            Console.WriteLine("Eingegebene Zahl : " + text);
+            Console.WriteLine("Eingegebene Zahl (als Text): " + text);
             Console.ReadLine();
 
-
-            Console.WriteLine("Bitte eine Zahl eingeben");
-            int num2 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Eingegebene Zahl " + num2); 
-
-
-
-
+            Console.ReadLine();
+            printAufgabe("2.2");
+            Console.WriteLine("Bitte eine Zahl eingeben und mit Enter bestätigen");
+            zahl = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Eingegebene Zahl nach Convert.ToInt32(text): " + zahl); 
             Console.ReadLine();
 
-
+            void printAufgabe(String s)
+            {
+                Console.WriteLine("\n\nAufgabe " + s);
+            }
 
         }
     }
