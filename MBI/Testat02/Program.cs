f5e837d436203812e6442601d07e37559233f9a9
Magdiel Martínez <magdieltercero@hotmail.com>
2020-03-04 16:00:06 +0100

    Update Program.cs

--- MBI/Testat02/Program.cs
+++ MBI/Testat02/Program.cs
@@ -1,12 +1,33 @@
-﻿using System;
+using System;
 
-namespace Testat02
+namespace Testat2
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int num = 1;
+            Console.WriteLine("i = " + num);
+
+            Console.ReadLine();
+
+            Console.WriteLine("Bitte eine Zahl eingeben und mit Enter bestätigen");
+            string text = Console.ReadLine();
+            Console.WriteLine("Eingegebene Zahl : " + text);
+            Console.ReadLine();
+
+
+            Console.WriteLine("Bitte eine Zahl eingeben");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Eingegebene Zahl " + num2); 
+
+
+
+
+            Console.ReadLine();
+
+
+
         }
     }
 }
